7e29103538f672af646e9f6c593dc0549e7844bf
Gino Valentino Pensuni <85497@fhwn.ac.at>
2015-06-17 10:42:44 +0200

    Added method for extracting the component from a byte array message

--- Client/Protocol.cs
+++ Client/Protocol.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommonInterfaces;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace Client
 {
-    class Protocol
+    public static class Protocol
     {
-        CommonInterfaces.
+        public static Component GetComponentFromMessage(byte[] msg)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            BinaryFormatter binForm = new BinaryFormatter();
+
+            memoryStream.Write(msg, 0, msg.Length);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            ComponentMessage message = (ComponentMessage)binForm.Deserialize(memoryStream);
+
+            return message.Component;
+        }
     }
 }
