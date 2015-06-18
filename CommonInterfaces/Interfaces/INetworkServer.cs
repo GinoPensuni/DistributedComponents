c6afed2c797759f8ec6580f8fe70f1f63cbc91bf
EduardRadoi <edmail27062014@gmail.com>
2015-06-18 10:11:39 +0200

    Interfaces Messages Events

---
+++ CommonInterfaces/Interfaces/INetworkServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInterfaces
+{
+    public interface INetworkServer
+    {
+        NetworkState ServerState
+        {
+            get;
+            set;
+        }
+
+
+        bool SendResult(List<object> Result, Guid id);
+
+        event EventHandler<ComponentRecievedEventArgs> RequestEvent;
+    }
+}
