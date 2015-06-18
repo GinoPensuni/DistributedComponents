425ed9d19d727caf264da78857edab93ddf10ad1
Gino Valentino Pensuni <85497@fhwn.ac.at>
2015-06-18 10:08:15 +0200

    Edited AddComponent

--- AddComponent/Add.cs
+++ AddComponent/Add.cs
@@ -17,30 +17,54 @@ namespace AddComponent
 
         private IEnumerable<string> outputHints;
 
+        public Add()
+        {
+            this.componentGuid = new Guid("82870640-2A79-4FE5-8F34-6075C0C4863F");
+
+            this.friendlyName = "Addition";
+
+            List<string> inputhints = new List<string>() {"System.Int32",
+                                                          "System.Int32"};
+            this.inputHints = inputhints;
+
+            List<string> outputhint = new List<string>() { "System.Int32" };
+
+            this.outputHints = outputhint;
+                                                          
+        }
 
         public Guid ComponentGuid
         {
-            get { }
+            get { return this.componentGuid;  } 
         }
 
         public string FriendlyName
         {
-            get { }
+            get { return this.friendlyName;  }
         }
 
         public IEnumerable<string> InputHints
         {
-            get { }
+            get { return this.inputHints; }
         }
 
         public IEnumerable<string> OutputHints
         {
-            get { }
+            get { return this.outputHints; }
         }
 
         public IEnumerable<object> Evaluate(IEnumerable<object> values)
         {
+            int sum = 0;
 
+            foreach (object value in values)
+            {
+                sum = sum + (int)value;
+            }
+
+            List<object> result = new List<object>() { sum };
+
+            return result;
         }
     }
 }
