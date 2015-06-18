c483572163a9c2462ee36a961a9105e15c053e9f
EduardRadoi <edmail27062014@gmail.com>
2015-06-18 10:56:28 +0200

    Added refereces logic

--- AppLogic/ClientLogic.cs
+++ AppLogic/ClientLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommonInterfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,27 @@ using System.Threading.Tasks;
 
 namespace AppLogic
 {
-    class ClientLogic
+    public class ClientLogic  : ILogic
     {
+        private static readonly INetworkClient client;
+        public Task<bool> DisconnectFromServer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveComponent(IComponent component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConnenctToServer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Tuple<Type, IComponent>>> LoadComponents()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
