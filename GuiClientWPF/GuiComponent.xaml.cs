39331b379a6815f943f2b1193c4cb78fb3a3997a
Bakkoon <patrick.braunstorfer@outlook.com>
2015-06-18 16:51:32 +0200

    Implemented lines as well as distinct parameter nodes

--- GuiClientWPF/GuiComponent.xaml.cs
+++ GuiClientWPF/GuiComponent.xaml.cs
@@ -20,11 +20,17 @@ namespace GuiClientWPF
     /// </summary>
     public partial class GuiComponent : UserControl
     {
+        public event EventHandler<ConnectionNodeClickedEventArgs> InputOutputNodeClicked;
+
+        private readonly List<InputNodeComponent> inputNodes;
+        private readonly List<InputNodeComponent> outputNodes;
         private Components entry;
 
         public GuiComponent()
         {
             InitializeComponent();
+            this.inputNodes = new List<InputNodeComponent>();
+            this.outputNodes = new List<InputNodeComponent>();
         }
 
         public GuiComponent(Components entry)
@@ -38,16 +44,41 @@ namespace GuiClientWPF
 
             foreach (var inputNode in entry.InputHints)
             {
-                var newInputNode = new InputNodeComponent(inputNode);
+                var newInputNode = new InputNodeComponent(inputNode, Direction.Left);
                 this.ComponentCanvas.Children.Add(newInputNode);
                 Canvas.SetTop(newInputNode, currentY);
-                Canvas.SetLeft(newInputNode, -1 * newInputNode.ActualWidth);
-                newInputNode.Margin = new Thickness(3);
+                Canvas.SetLeft(newInputNode, -1 * newInputNode.ActualWidth - 5);
+                //newInputNode.Margin = new Thickness(3);
                 currentY += newInputNode.ActualHeight + 3;
                 largestExcessWidth = newInputNode.ActualWidth > largestExcessWidth ? newInputNode.ActualWidth : largestExcessWidth;
+                newInputNode.ConnectionNodeClicked += InputOutputNode_ConnectionNodeClicked;
+                this.inputNodes.Add(newInputNode);
+            }
+
+            currentY = 0.0;
+
+            foreach (var outputNode in entry.OutputHints)
+            {
+                var newOutputNode = new InputNodeComponent(outputNode, Direction.Right);
+                this.ComponentCanvas.Children.Add(newOutputNode);
+                Canvas.SetTop(newOutputNode, currentY);
+                Canvas.SetLeft(newOutputNode, this.Width + 5);
+                //newOutputNode.Margin = new Thickness(3);
+                currentY += newOutputNode.ActualHeight + 3;
+                largestExcessWidth = newOutputNode.ActualWidth > largestExcessWidth ? newOutputNode.ActualWidth : largestExcessWidth;
+                newOutputNode.ConnectionNodeClicked += InputOutputNode_ConnectionNodeClicked;
+                this.outputNodes.Add(newOutputNode);
             }
 
             this.Width += largestExcessWidth * 2;
+        }
+
+        private void InputOutputNode_ConnectionNodeClicked(object sender, ConnectionNodeClickedEventArgs e)
+        {
+            if (this.InputOutputNodeClicked != null)
+            {
+                this.InputOutputNodeClicked(this, e);
+            }
         }
     }
 }
--- GuiClientWPF/InputOutputComponentNode.xaml.cs
+++ GuiClientWPF/InputOutputComponentNode.xaml.cs
@@ -20,12 +20,33 @@ namespace GuiClientWPF
     /// </summary>
     public partial class InputNodeComponent : UserControl
     {
-        public InputNodeComponent(string fullTypeName)
+        public event EventHandler<ConnectionNodeClickedEventArgs> ConnectionNodeClicked;
+
+        public InputNodeComponent(string fullTypeName, Direction drawDirection)
         {
             InitializeComponent();
+
+            switch (drawDirection)
+            {
+                case Direction.Left:
+                    Grid.SetColumn(this.TypeLabel, 1);
+                    Grid.SetColumn(this.Marker, 0);
+                    break;
+                case Direction.Right:
+                    break;
+            }
+
             this.TypeLabel.Text = fullTypeName;
             this.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
             this.Arrange(new Rect(0, 0, this.DesiredSize.Width, this.DesiredSize.Height));
+        }
+
+        private void Marker_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (this.ConnectionNodeClicked != null)
+            {
+                this.ConnectionNodeClicked(this, new ConnectionNodeClickedEventArgs(this, (Ellipse)sender));
+            }
         }
     }
 }
