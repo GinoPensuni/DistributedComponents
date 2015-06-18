28193b56501dc6a37f681157c877638bee6f5f77
EduardRadoi <edmail27062014@gmail.com>
2015-06-18 08:58:15 +0200

    Network State added

---
+++ CommonInterfaces/Enums/NetworkState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInterfaces
+{
+    public enum NetworkState
+    {
+        /// <summary>
+        /// Server client communication established.
+        /// </summary>
+        Running = 1,
+
+        /// <summary>
+        /// Error during communication between client and Server.
+        /// </summary>
+        Error = 2,
+
+        /// <summary>
+        /// Communication between 
+        /// </summary>
+        Stopped = 3,
+    }
+}
