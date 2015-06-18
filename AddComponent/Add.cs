e52d83ceda7715b730da8d6dec74dd627916006b
Gino Valentino Pensuni <85497@fhwn.ac.at>
2015-06-18 09:53:37 +0200

    Added AddComponent to track

---
+++ AddComponent/Add.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonInterfaces;
+
+namespace AddComponent
+{
+    public class Add : IComponent
+    {
+        private Guid componentGuid;
+
+        private string friendlyName;
+
+        private IEnumerable<string> inputHints;
+
+        private IEnumerable<string> outputHints;
+
+
+        public Guid ComponentGuid
+        {
+            get { }
+        }
+
+        public string FriendlyName
+        {
+            get { }
+        }
+
+        public IEnumerable<string> InputHints
+        {
+            get { }
+        }
+
+        public IEnumerable<string> OutputHints
+        {
+            get { }
+        }
+
+        public IEnumerable<object> Evaluate(IEnumerable<object> values)
+        {
+
+        }
+    }
+}
