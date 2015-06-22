d63a6dcc28f2771c45a662cba7f931b1da31ea9a
Bakkoon <patrick.braunstorfer@outlook.com>
2015-06-22 11:45:38 +0200

    Created ResultReceivedEventArgs

---
+++ CommonInterfaces/EvemtArgs/ResultReceivedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonRessources.Interfaces
+{
+    public class ResultReceivedEventArgs
+    {
+        public Guid JobGuid { get; set; }
+
+        public IEnumerable<object> Results { get; set; }
+    }
+}
