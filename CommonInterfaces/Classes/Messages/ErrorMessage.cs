9b34d8b72adda04a23850a4b10b54bebce050016
JuergenFassl <94018@fhwn.ac.at>
2015-06-25 10:19:12 +0200

    Added errormessage

---
+++ CommonInterfaces/Classes/Messages/ErrorMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonRessources
+{
+    [Serializable]
+    public class ErrorMessage : Message
+    {
+        public ErrorMessage(MessageType type, Guid ID) : base(ID,type)
+        {
+        }
+        public ErrorMessage(Guid ID)
+            : this(MessageType.Unknown, ID)
+        {
+        }
+
+        public Guid JobRequestGuid { get; set; }
+
+        public Exception Exception {get; set;}
+    }
+}
