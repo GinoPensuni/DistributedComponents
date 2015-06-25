193542c1f62f95e05b438152b1906bec17d36fc0
Markus Hofer <mhofer4991@gmail.com>
2015-06-25 13:47:06 +0200

    Documented a little bit

--- CommonInterfaces/Classes/Messages/ErrorMessage.cs
+++ CommonInterfaces/Classes/Messages/ErrorMessage.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace CommonRessources
 {
+    /// <summary>
+    /// This message is only for errors on job request level (not single atomic component execution).
+    /// </summary>
     [Serializable]
     public class ErrorMessage : Message
     {
